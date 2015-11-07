457d76c372539131681546f8b448e54e719c30b4
Anmol Singh Jaggi <anmolj.65@gmail.com>
2015-11-07 19:07:40 +0530

    Removed the login system in the CLI UI

--- with-DB/graphDB/UI/cli/main.py
+++ with-DB/graphDB/UI/cli/main.py
@@ -1,30 +1,43 @@
 from include.cypher_interface import *
 
 def main():
-    userID = raw_input("Enter your User ID: ")
-    if not is_userID_present(userID):
-        print "userID not found!!"
-        return -1
+    menu = "\n1. Get recommendations\n2. List all the movies\n3. List the movies rated\n4. Update rating\n"
     
-    menu = "\n1. Get recommendations\n2. List all the movies\n3. List the movies you have rated\n4. Update rating\n"
     while True:
         option = int(raw_input(menu))
+        
         if option is 1:
+            userID = raw_input("Enter your user ID: ")
+            if not is_userID_present(userID):
+                print "userID not found!!"
+                continue
             print get_recommendations(userID)
+        
         elif option is 2:
             print show_movies()
+        
         elif option is 3:
+            userID = raw_input("Enter your user ID: ")
+            if not is_userID_present(userID):
+                print "userID not found!!"
+                continue
             print show_ratings(userID)
+        
         elif option is 4:
-            movieID = raw_input("Enter the ID of the movie: ")
+            userID = raw_input("Enter your user ID: ")
+            if not is_userID_present(userID):
+                print "userID not found!!"
+                continue
+            movieID = raw_input("Enter the movie ID: ")
             if not is_movieID_present(movieID):
-                print "Movie not found!!"
+                print "movieID not found!!"
                 continue
             rating = raw_input("Enter new rating [1-10]: ")
             if not (int(rating) > 0 and int(rating) < 11):
                 print "Invalid rating!!"
                 continue
             print update_rating(userID, movieID, rating)
+        
         else:
             print "Invalid option!!"
             return -1
