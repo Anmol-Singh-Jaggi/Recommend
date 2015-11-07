d317269c4c54563b8d570840cfaf0752e5f9eea6
Anmol Singh Jaggi <anmolj.65@gmail.com>
2015-11-07 13:37:16 +0530

    Fixed gitignore

--- with-DB/graphDB/UI/cli/include/paths.py
+++
@@ -1,3 +0,0 @@
-CYPHER_SCRIPTS_PATH = "/media/Data/anmol/coding/github/Recommend/with-DB/graphDB/cypher/"
-NEO4J_SHELL_PATH = "/media/Data/anmol/coding/github/Recommend/with-DB/graphDB/neo4j/bin/neo4j-shell"
-
