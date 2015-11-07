523ad24e4f93dd8c7069e459af6981e996fa325f
Anmol Singh Jaggi <anmolj.65@gmail.com>
2015-11-07 14:40:04 +0530

    Made the joining of the cypher script path with the cypher script name happen in craft_cypher_command() itself

--- with-DB/graphDB/UI/cli/include/common.py
+++ with-DB/graphDB/UI/cli/include/common.py
@@ -1,7 +1,8 @@
 import subprocess
 from paths import *
 
-def craft_cypher_command(cypher_script_path, *args):
+def craft_cypher_command(cypher_script_name, *args):
+    cypher_script_path = CYPHER_SCRIPTS_PATH + cypher_script_name
     cypher_script = open(cypher_script_path).read()
     arg_index = 1
     for arg in args:
@@ -10,6 +11,5 @@ def craft_cypher_command(cypher_script_path, *args):
     return cypher_script
 
 def execute_neo4j_shell(cypher_script_name, *args):
-    cypher_script_path = CYPHER_SCRIPTS_PATH + cypher_script_name
-    cypher_command = craft_cypher_command(cypher_script_path, *args)
+    cypher_command = craft_cypher_command(cypher_script_name, *args)
     return subprocess.check_output([NEO4J_SHELL_PATH, "-c", cypher_command])
