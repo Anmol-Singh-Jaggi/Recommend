b41de1e830e98f39868d08fed3ea3ffa84b2991c
Anmol Singh Jaggi <anmolj.65@gmail.com>
2015-11-08 17:47:08 +0530

    Changed the web UI input mechanism to GET parameters

--- with-DB/graphDB/UI/web/NeoRecApp/urls.py
+++ with-DB/graphDB/UI/web/NeoRecApp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^get_reco/(?P<userID>[0-9]+)/$', views.get_recommendations, name='get_reco'),
-    url(r'^view_ratings/(?P<userID>[0-9]+)/$', views.view_ratings, name='view_ratings'),
-    url(r'^update_rating/(?P<userID>[0-9]+)/(?P<movieID>[0-9]+)/(?P<rating>[0-9]+)/$', views.update_rating, name='update_rating'),
+    url(r'^get_reco/$', views.get_recommendations, name='get_reco'),
+    url(r'^view_ratings/$', views.view_ratings, name='view_ratings'),
+    url(r'^update_rating/$', views.update_rating, name='update_rating'),
     url(r'^list_movies/$', views.list_movies, name='list_movies'),
 ]
--- with-DB/graphDB/UI/web/NeoRecApp/views.py
+++ with-DB/graphDB/UI/web/NeoRecApp/views.py
@@ -7,21 +7,22 @@ def index(request):
     graph = Graph()
     
     usage = []
-    usage.append(["/get_reco", "/userID", "Get recommendations for the given userID"])
-    usage.append(["/view_ratings", "/userID", "Get the list of movies rated by the given userID"])
-    usage.append(["/update_rating", "/userID/movieID/rating", "Update rating for movieID by the given userID"])
-    usage.append(["/list_movies", "", "List all the movies present in the database"])
+    usage.append(["/get_reco?userid=<int>", "Get recommendations for the given userID"])
+    usage.append(["/view_ratings?userid=<int>", "Get the list of movies rated by the given userID"])
+    usage.append(["/update_rating?userid=<int>&movieid=<int>&rating=<int>", "Update rating for movieID by the given userID"])
+    usage.append(["/list_movies", "List all the movies present in the database"])
     
     response_dict = {}
     response_dict["title"] = "Welcome to the NeoRec Recommendation Engine!!"
     response_dict["table_caption"] = "Usage"
-    response_dict["table_headers"] = ["Option", "Parameters", "Comments"]
+    response_dict["table_headers"] = ["Query string", "Action"]
     response_dict["table_rows"] = usage
     
     return render(request, "render_table.html", response_dict)
 
-def get_recommendations(request, userID):
-    if not is_userID_present(userID):
+def get_recommendations(request):
+    userID = request.GET.get("userid")
+    if not (userID and is_userID_present(userID)):
         return HttpResponse("<b>Error:</b> UserID '" + str(userID) + "' not found")
     
     graph = Graph()
@@ -49,8 +50,9 @@ def get_recommendations(request, userID):
     
     return render(request, "render_table.html", response_dict)
 
-def view_ratings(request, userID):
-    if not is_userID_present(userID):
+def view_ratings(request):
+    userID = request.GET.get("userid")
+    if not (userID and is_userID_present(userID)):
         return HttpResponse("<b>Error:</b> UserID '" + str(userID) + "' not found")
     
     graph = Graph()
@@ -74,11 +76,16 @@ def view_ratings(request, userID):
     
     return render(request, "render_table.html", response_dict)
 
-def update_rating(request, userID, movieID, rating):
-    if not is_userID_present(userID):
+def update_rating(request):
+    userID = request.GET.get("userid")
+    if not (userID and is_userID_present(userID)):
         return HttpResponse("<b>Error:</b> UserID '" + str(userID) + "' not found")
-    if not is_movieID_present(movieID):
+    
+    movieID = request.GET.get("movieid")
+    if not(movieID and is_movieID_present(movieID)):
         return HttpResponse("<b>Error:</b> MovieID '" + str(movieID) + "' not found")
+    
+    rating = request.GET.get("rating")
     if not (int(rating) >=1 and int(rating) <= 10):
         return HttpResponse("<b>Error:</b> Invalid rating value '" + str(rating) + "'")
     
